fa7dae674dfe723eb361b99e1f4ae56757319e40
Mokens <miquiangelhotmail@gmail.com>
2025-02-20 02:40:45 -0300

    style

--- src/app/home/components/HotelCard.jsx
+++ src/app/home/components/HotelCard.jsx
@@ -1,34 +1,38 @@
-import { Link } from 'react-router'
-import { IoLocationOutline } from 'react-icons/io5'
-import { priceFormat } from '../../../utils'
-import StarRating from '../../../components/ui/StarRating'
+import { Link } from 'react-router';
+import { IoLocationOutline } from 'react-icons/io5';
+import { priceFormat } from '../../../utils';
+import StarRating from '../../../components/ui/StarRating';
 
-function HotelCard ({ id, name, rating, city, country, price, image }) {
-  return (
-    <div className='flex flex-col bg-white rounded-lg shadow-lg overflow-hidden hover:scale-105 transition-transform duration-300'>
-      <img
-        className='aspect-[1.4] object-cover overflow-hidden'
-        src={image}
-        alt={name}
-      />
-      <div className='p-5 grow'>
-        <h2 className='text-lg font-semibold'>{name}</h2>
-        <div className='mb-1'>
-          <StarRating rating={rating} />
-        </div>
-        <p className='flex items-center gap-1 mb-1'>
-          <IoLocationOutline /> <span className='text-sm'>{city}, {country}</span>
-        </p>
-        <p className='font-semibold'>{priceFormat(price)}</p>
-      </div>
-      <div className='p-5 bg-gray-50'>
-        <Link
-          to={`/hotel/${id}`}
-          className='btn block text-center text-white bg-blue-500'>
-          More Info
-        </Link>
-      </div>
-    </div>
-  )
+function HotelCard({ id, name, rating, city, country, price, image }) {
+	return (
+		<div className="flex flex-col bg-white rounded-lg shadow-lg overflow-hidden hover:scale-110 transition-transform duration-380">
+			<img
+				className="aspect-[1.4] object-cover overflow-hidden"
+				src={image}
+				alt={name}
+			/>
+			<div className="p-5 grow">
+				<h2 className="text-lg font-semibold">{name}</h2>
+				<div className="mb-1">
+					<StarRating rating={rating} />
+				</div>
+				<p className="flex items-center gap-1 mb-1">
+					<IoLocationOutline />{' '}
+					<span className="text-sm">
+						{city}, {country}
+					</span>
+				</p>
+				<p className="font-semibold">{priceFormat(price)}</p>
+			</div>
+			<div className="p-5 bg-gray-50">
+				<Link
+					to={`/hotel/${id}`}
+					className="btn block text-center text-white bg-blue-500"
+				>
+					More Info
+				</Link>
+			</div>
+		</div>
+	);
 }
-export default HotelCard
+export default HotelCard;
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,49 +1,63 @@
-import { useState } from 'react'
-import { Link } from 'react-router'
-import { GiHamburgerMenu } from "react-icons/gi"
-import { useAuth } from '../../hooks/useAuth'
-import Logo from './Logo'
-import Menu from '../ui/Menu'
+import { useState } from 'react';
+import { Link } from 'react-router';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { useAuth } from '../../hooks/useAuth';
+import Logo from './Logo';
+import Menu from '../ui/Menu';
 
-function Header () {
-  const [open, setOpen] = useState(false)
-  const { isAuth, logout } = useAuth()
+function Header() {
+	const [open, setOpen] = useState(false);
+	const { isAuth, logout } = useAuth();
 
-  return (
-    <div className='sticky top-0 w-full bg-white shadow-lg z-10'>
-      <div className='max-w-5xl max-[1024px]:px-4 mx-auto flex items-center justify-between h-20'>
-        <Logo />
+	return (
+		<div className="sticky top-0 w-full bg-aliceblue shadow-lg z-10">
+			<div className="max-w-5xl max-[1024px]:px-4 mx-auto flex items-center justify-between h-20">
+				<Logo />
 
-        <Menu open={open} setOpen={setOpen}>
-          <nav className='flex flex-col sm:flex-row items-center gap-4'>
-            {isAuth ? (<>
-              <Link to='/reservations' className='text-lg hover:text-blue-500 transition-colors duration-300 ease-in-out'>
-                Reservations
-              </Link>
-              <button
-                className='btn bg-red-400 text-white hover:bg-red-500 transition-colors duration-300 ease-in-out'
-                onClick={() => logout()}>
-                Logout
-              </button>
-            </>) : (<>
-              <Link to='/login' className='text-lg hover:text-blue-500 transition-colors duration-300 ease-in-out'>
-                Login
-              </Link>
-              <Link to='/register' className='text-lg hover:text-blue-500 transition-colors duration-300 ease-in-out'>
-                Register
-              </Link>
-            </>)}
-          </nav>
-        </Menu>
+				<Menu open={open} setOpen={setOpen}>
+					<nav className="flex flex-col sm:flex-row items-center gap-4">
+						{isAuth ? (
+							<>
+								<Link
+									to="/reservations"
+									className="text-lg hover:text-blue-500 transition-colors duration-300 ease-in-out"
+								>
+									Reservations
+								</Link>
+								<button
+									className="btn bg-red-400 text-white hover:bg-red-500 transition-colors duration-300 ease-in-out"
+									onClick={() => logout()}
+								>
+									Logout
+								</button>
+							</>
+						) : (
+							<>
+								<Link
+									to="/login"
+									className="text-lg hover:text-blue-500 transition-colors duration-300 ease-in-out"
+								>
+									Login
+								</Link>
+								<Link
+									to="/register"
+									className="text-lg hover:text-blue-500 transition-colors duration-300 ease-in-out"
+								>
+									Register
+								</Link>
+							</>
+						)}
+					</nav>
+				</Menu>
 
-        <button
-          className='min-sm:hidden cursor-pointer'
-          onClick={() => setOpen(true)}
-        >
-          <GiHamburgerMenu className='size-7' />
-        </button>
-      </div>
-    </div>
-  )
+				<button
+					className="min-sm:hidden cursor-pointer"
+					onClick={() => setOpen(true)}
+				>
+					<GiHamburgerMenu className="size-7" />
+				</button>
+			</div>
+		</div>
+	);
 }
-export default Header
+export default Header;
